f4a05838c4195c303bc1d64e510fb9be82096030
cloudcoke <lms4296@gmail.com>
2023-03-16 23:43:54 +0900

    thunk 미들웨어 장착

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,5 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-export const store = createStore(rootReducer, composeWithDevTools());
+const isProduction = process.env.NODE_ENV === "production" ? applyMiddleware(thunk) : composeWithDevTools(applyMiddleware(thunk));
+export const store = createStore(rootReducer, isProduction);
