e5f20a6be12cc8f20ccbf0778131081bc06517c8
cloudcoke <lms4296@gmail.com>
2023-03-16 23:58:24 +0900

    redux persist 설정 완료

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -2,6 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "login",
+  storage,
+  whitelist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const isProduction = process.env.NODE_ENV === "production" ? applyMiddleware(thunk) : composeWithDevTools(applyMiddleware(thunk));
-export const store = createStore(rootReducer, isProduction);
+export const store = createStore(persistedReducer, isProduction);
+export const persistor = persistStore(store);
